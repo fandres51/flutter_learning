c722621ced532db4d57ea20ee26d293c755dcf7e
Fabio Enríquez <fabio.and1514@gmail.com>
2021-09-30 20:35:02 -0500

    world app

---
+++ project2_world_time_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:project2_world_time_app/pages/home.dart';
+import 'package:project2_world_time_app/pages/loading.dart';
+import 'package:project2_world_time_app/pages/choose_locartion.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
---
+++ project2_world_time_app/lib/pages/choose_locartion.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        elevation: 0.0,
+      ),
+      body: Text('Choose location screen'),
+    );
+  }
+}
